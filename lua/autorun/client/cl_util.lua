1154cbb2285d01a972412ac58f76d0717b72dda4
LLYDIA <lewisrobertlancaster@gmail.com>
2021-08-19 16:40:04 +0100

    Delete lua/autorun/client directory

--- lua/autorun/client/cl_util.lua
+++
@@ -1,7 +0,0 @@
-
-function MediaPlayer.CreateWarningBox(title, message, timeout)
-    timeout = timeout or false
-
-    MediaPlayer.ReinstantiatePanel("WarningBox")
-    MediaPlayer.GetPanel("WarningBox"):SetWarning(title, message, timeout)
-end
